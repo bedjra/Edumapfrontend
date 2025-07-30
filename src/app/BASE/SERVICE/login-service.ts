34792b0aa565c4b7e76fde10dc7ad8f4d7610153
Armel <armelbedjra12@gmail.com>
2025-07-30 23:06:59 +0000

    Description

--- src/app/BASE/SERVICE/login-service.ts
+++ src/app/BASE/SERVICE/login-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
@@ -8,7 +9,11 @@ export class LoginService {
 
   constructor(private http: HttpClient) {}
 
-  login(credentials: { email: string; password: string }) {
+  login(credentials: { email: string; password: string }): Observable<any> {
     return this.http.post(`${this.baseUrl}/user/login`, credentials);
+  }
+
+  registerUser(data: { email: string; password: string; role: string }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/user/save`, data);
   }
 }
