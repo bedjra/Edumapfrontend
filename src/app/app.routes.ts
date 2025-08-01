8a80b4c2785a26f233636ac2eee3a26093ef7c2e
Armel <armelbedjra12@gmail.com>
2025-08-01 17:20:08 +0000

    Description

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { LoginComponent } from './BASE/CONNEXION/login/login';
 import { BaseComponent } from './BASE/PAGES/base/base.component';
 import { DashboardComponent } from './BASE/PAGES/dashboard/dashboard.component';
 import { Parametres } from './BASE/PAGES/parametres/parametres';
+import { Paiement } from './BASE/PAGES/paiement/paiement';
+import { Add } from './BASE/PAGES/Eleve/add/add';
+import { Liste } from './BASE/PAGES/Eleve/liste/liste';
+import { Note } from './BASE/PAGES/Note/note/note';
+import { Buletin } from './BASE/PAGES/Note/buletin/buletin';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -17,6 +22,12 @@ export const routes: Routes = [
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'parametre', component: Parametres },
+      { path: 'paiement', component: Paiement },
+      { path: 'ajouter', component: Add },
+      { path: 'liste', component: Liste },
+       { path: 'note', component: Note },
+      { path: 'bulletin', component: Buletin },
+      
       
     ],
   },
