89ebbacde917e59b87d17df801234223ba9b35dd
Armel <armelbedjra12@gmail.com>
2025-08-01 17:24:48 +0000

    Description

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router'; // <-- RouterModule ajouté ici
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-
-// ⚠️ Ce module est indispensable pour utiliser ngModel
 import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
@@ -13,8 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    importProvidersFrom(FormsModule) ,
-    provideHttpClient()      // ✅ Fournit HttpClient globalement
-
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(RouterModule), // ✅ ajoute cette ligne
+    provideHttpClient()
   ]
 };
