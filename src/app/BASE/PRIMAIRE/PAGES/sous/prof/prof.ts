f08ab38a18f97afb6f6eaa8373b40303c50c1537
Armel <armelbedjra12@gmail.com>
2025-08-02 22:10:18 +0000

    liste pri?QIRE start

---
+++ src/app/BASE/PRIMAIRE/PAGES/sous/prof/prof.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prof',
+  imports: [],
+  templateUrl: './prof.html',
+  styleUrl: './prof.css'
+})
+export class Prof {
+
+}
---
+++ src/app/BASE/PRIMAIRE/PAGES/sous/scolarite/scolarite.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scolarite',
+  imports: [],
+  templateUrl: './scolarite.html',
+  styleUrl: './scolarite.css'
+})
+export class Scolarite {
+
+}
