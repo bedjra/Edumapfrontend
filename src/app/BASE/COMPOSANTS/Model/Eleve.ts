4a39260f43af22f54f10b10204f5cfb74ea4b87b
Armel <armelbedjra12@gmail.com>
2025-08-02 00:49:29 +0000

    version base fonde

---
+++ src/app/BASE/COMPOSANTS/Model/Eleve.ts
@@ -0,0 +1,20 @@
+export interface Eleve {
+  id?: number;
+  nom: string;
+  prenom: string;
+  adresse: string;
+  matricule: string;
+  classe: 'CP1' | 'CP2' | 'CE1' | 'CE2' | 'CM1' | 'CM2'; // adapte selon ton enum Java
+  sexe: 'Masculin' | 'Féminin';
+  lieuNais: string;
+  etblProv: string;
+  nationnalite: string;
+  dateNaiss: string; // format ISO : 'YYYY-MM-DD'
+  photo?: string | ArrayBuffer | null;
+
+  // Tuteur
+  tuteurNom: string;
+  tuteurPrenom: string;
+  tuteurTelephone: string;
+  tuteurProfession: string;
+}
--- src/app/BASE/SERVICE/etudiant-service.ts
+++ src/app/BASE/SERVICE/etudiant-service.ts
@@ -2,39 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
+import { Eleve } from '../COMPOSANTS/Model/Eleve';
 
 @Injectable({
   providedIn: 'root',
 })
 
-export interface EleveDto {
-  id?: number;
-  nom: string;
-  prenom: string;
-  adresse: string;
-  matricule: string;
-  classe: 'CP1' | 'CP2' | 'CE1' | 'CE2' | 'CM1' | 'CM2'; // adapte selon ton enum Java
-  sexe: 'Masculin' | 'Féminin';
-  lieuNais: string;
-  etblProv: string;
-  nationnalite: string;
-  dateNaiss: string; // LocalDate → string en ISO format "YYYY-MM-DD"
-  photo?: string | ArrayBuffer | null; // base64 ou FileReader result
-
-  // Tuteur
-  tuteurNom: string;
-  tuteurPrenom: string;
-  tuteurTelephone: string;
-  tuteurProfession: string;
-}
 
 export class EtudiantService {
-  private baseUrl = environment.apiUrl;
+private baseUrl = `${environment.apiUrl}/eleve`;
 
   constructor(private http: HttpClient) {}
 
-    enregistrerEtudiant(etudiant: EleveDto): Observable<EleveDto> {
-    return this.http.post<EleveDto>(this.baseUrl, etudiant);
+  enregistrerEtudiant(etudiant: Eleve): Observable<Eleve> {
+    return this.http.post<Eleve>(this.baseUrl, etudiant);
   }
-
 }
