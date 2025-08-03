aec08ac6239f3015579b6ac4eb4d880f8bf8eaf2
Armel <armelbedjra12@gmail.com>
2025-08-03 19:03:13 +0000

    Description

--- src/app/BASE/PRIMAIRE/PAGES/parametres/parametres.ts
+++ src/app/BASE/PRIMAIRE/PAGES/parametres/parametres.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { User } from '../sous/user/user';
+import { Scolarite } from '../sous/scolarite/scolarite';
+import { Prof } from '../sous/prof/prof';
 
 @Component({
   selector: 'app-parametres',
-  imports: [],
+  imports: [User,Scolarite,Prof],
   templateUrl: './parametres.html',
   styleUrl: './parametres.css'
 })
