68c78da6a098f5c85244eca8de8546f760be0b6d
Armel <armelbedjra12@gmail.com>
2025-08-11 21:41:50 +0000

    Description

---
+++ src/app/BASE/PRIMAIRE/PAGES/Note/note7/note7.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note7',
+  imports: [],
+  templateUrl: './note7.html',
+  styleUrl: './note7.css'
+})
+export class Note7 {
+
+}
