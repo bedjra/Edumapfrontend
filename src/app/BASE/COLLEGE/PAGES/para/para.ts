de07f58cc44ca8ae11b203568f557d47bc8e982a
Armel <armelbedjra12@gmail.com>
2025-08-12 06:45:36 +0000

    Description

---
+++ src/app/BASE/COLLEGE/PAGES/para/para.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-para',
+  imports: [],
+  templateUrl: './para.html',
+  styleUrl: './para.css'
+})
+export class Para {
+
+}
